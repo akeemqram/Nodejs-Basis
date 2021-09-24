6bc70cc288d0f2fe0fb2cb5c62adc2d44ed4e159
Samuel Ogu <hello@samuelogu.com>
2021-09-24 16:00:39 +0100

    variables and data types

---
+++ app.js
@@ -0,0 +1,13 @@
+let name = "Samuel Ogu"; //string //single line comment
+let location = 'Atiku';
+let age = 23; //integer
+let decimal = 2456.78; // float
+let boolean = true; // or false // boolean
+
+/*
+Multiple line comment
+ */
+
+console.log("My name is " + name  + ". \nMy age is "+age) //concatenation
+console.log(name+" is a boy")
+console.log(age)
