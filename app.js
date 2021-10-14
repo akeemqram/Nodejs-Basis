44ee8bf278d46e1300f61c7369d3ab7d5e024eaf
akeemyakubu <akeemzy2@gmail.com>
2021-10-14 16:28:31 +0100

    first push

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-let name = "Samuel Ogu"; //string //single line comment
+let name="Samuel Ogu"; //string //single line comment
 let location = 'Atiku';
 let age = 23; //integer
 let decimal = 2456.78; // float
-let boolean = true; // or false // boolean
+let boolean = false; // or false // boolean
+
 
 /*
 Multiple line comment
@@ -10,4 +11,13 @@ Multiple line comment
 
 console.log("My name is " + name  + ". \nMy age is "+age) //concatenation
 console.log(name+" is a boy")
+console.log(decimal)
+console.log(location)
 console.log(age)
+console.log("I scored "+ decimal +" in my video game test");
+console.log(decimal);
+console.log(name)
+console.log(boolean)
+console.log("Akeem who is " +age+ " years old, works for "+name +" at " +location+ " but will be leaving soon for service on the " +decimal+ " this week")
+
+
